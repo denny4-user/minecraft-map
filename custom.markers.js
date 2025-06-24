2c7bd5445b4ab29f4e433d54f5e7dac2555c9825
denny4_user <82503296+denny4-user@users.noreply.github.com>
2025-06-24 12:15:23 +0300

    Test markers point

--- custom.markers.js
+++ custom.markers.js
@@ -38,47 +38,23 @@ Use the imageScale property if the pin image is too large.
 
 UnminedCustomMarkers = {
 
-    isEnabled: false,
+    isEnabled: true,
 
     markers: [
 
-        // Example 1: Simple marker
-        {
-            x: -200,
-            z: -200,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-        },
-
         // Example 2: Marker with text
         {
-            x: 0,
-            z: 0,
+            x: 1867,
+            z: 7610,
             image: "custom.pin.png",
             imageAnchor: [0.5, 1],
             imageScale: 0.5,
-            text: "Marker with text",
-            textColor: "red", 
+            text: "Нора Чубрилы",
+            textColor: "white", 
             offsetX: 0,
             offsetY: 20,
             font: "bold 20px Calibri,sans serif",
-        },
-
-        // Example 3: Text only
-        {
-            x: 200,
-            z: 200,
-            text: "Text only", 
-            textColor: "yellow", 
-            offsetX: 0,
-            offsetY: 0, 
-            font: "bold 50px Calibri,sans serif",
-        },
-
-        // add your markers here
-
-
+        }
 
 
         // do not delete the following two closing brackets
